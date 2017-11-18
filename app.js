d6c3aebe5e2d24b88567081087a65a6768cdb989
Kaleo <kaleo@devicebook.com>
2017-11-18 04:20:19 -0800

    Separate out WobblyShape, audioPlayer, controls, lighting, and settings. Also add favicon, though it doesn't seem to be working (?)

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
-console.log('u wot m8');
-
 requirejs.config({
     baseUrl: "lib/modules",
     paths: {
         "jquery": "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min",
         "datGui": "../dat.gui.min",
-        "detector": "../Detector",
+        "detector": "../detector",
         "fpsMeter": "../fpsmeter.min",
         "threejs": "../three",
         "trackballControls": "../threejs-trackballcontrols",
         "threeJsHelpers": "../threejshelpers",
-
-        // "wobblybeat": "lib/modules/wobblybeat",
-        // "test": "lib/modules/test",
     }
 });
-console.log('u wot m9');
 
 // require(['test']);
 require(['wobblybeat']);
